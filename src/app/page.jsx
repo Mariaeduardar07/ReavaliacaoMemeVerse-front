a05d66f4a4dd76748bdafc7c94e99370b0d98293
Maria Eduarda <mariaeduardareisTDS2.senai@gmail.com>
2025-06-02 15:52:25 -0300

    feat: create hero section components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import HeroSection from "@/components/heroSection";
 
 export default function Home() {
   // Array de dados dos memes para serem passados como props
@@ -168,8 +169,13 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-     <Header />
-     <Footer />
+      <Header />
+      <div className={styles.mainContent}>
+        <div className={styles.contentArea}>
+          <HeroSection memeOfTheDay={memeOfTheDay} />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
